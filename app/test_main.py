324fa806f66963d8ed79f11f7cf139635a85b6e8
serhii_chaban <boetz1995@gmail.com>
2024-04-14 18:43:35 +0300

    solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "string,result",
+    [
+        pytest.param("playgrounds", True, id="no repeated letters"),
+        pytest.param("look", False, id="there are repeated letters"),
+        pytest.param("Adam", False, id=("there are repeated "
+                                        "letters with Upper Case")),
+        pytest.param("", True, id="empty string is isogram"),
+    ]
+)
+def test_is_isogram(string: str, result: bool) -> None:
+    assert is_isogram(string) == result
